06dae3bed07dd97e9eccc45c8323322a1fa03381
Gauri Sahane <86250098+Gaurisahane29@users.noreply.github.com>
2021-06-22 13:01:52 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,9 @@
-package exam;
-
-public class ConvertToBinary {
-
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+StringBuilder result=new
+  StringBuilder();
+for(int i=31;i>=0;i--)
+{
+  int mask=1<<i;
+  result.append((x&mask)!=0?
+                1:0);
 }
+Return result.toString();
